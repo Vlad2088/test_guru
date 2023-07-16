444f8954a408156b98fadbc9172fc18c71a9ab10
Vlad2088 <vladislavganieff@yandex.ru>
2023-07-17 02:39:10 +1200

    filling in the seed.rb file and adding sorting methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.sort_tests_by_category(title)
+    includes(:category)
+      .where(categories: { title: title})
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def sorting_tests_by_level(level)
+    tests.where(level: level)
+  end
 end
