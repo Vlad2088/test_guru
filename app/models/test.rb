33d65a1bf54d48c08380a115aef55eb017532626
Vlad2088 <vladislavganieff@yandex.ru>
2023-07-20 17:34:20 +1200

    refactoring use inner join for sorting methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,8 @@
 class Test < ApplicationRecord
-  #belongs_to :category
-  #has_and_belongs_to_many :users
-
   def self.sort_tests_by_category(title)
     joins('INNER JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title = ?', title)
-      .order('tests.title DESC')
+      .where(categories: { title: title})
+      .order(title: :desc)
       .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  #has_and_belongs_to_many :tests
-
-  def sorting_tests_by_level(level)
+  def tests_by_level(level)
     Test.joins('INNER JOIN tests_users ON tests.id = tests_users.test_id')
       .joins('INNER JOIN users ON users.id = tests_users.user_id')
-      .where('tests.level = ? AND users.id = ?', level, id)
+      .where('tests.level = ?', level)
   end
 end
