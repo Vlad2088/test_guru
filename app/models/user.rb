d5f3a8b355348cdf39ce565727a49a05cb6f43e4
Vlad2088 <vladislavganieff@yandex.ru>
2023-07-20 22:38:45 +1200

    refactoring sorting methods tests by level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins('INNER JOIN tests_users ON tests.id = tests_users.test_id')
-      .joins('INNER JOIN users ON users.id = tests_users.user_id')
-      .where('tests.level = ?', level)
+      .where(tests: { level: level}, tests_users: { user_id: id})
   end
 end
