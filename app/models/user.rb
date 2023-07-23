483da8a4215c6aa8dc55c0c385f0aa73ebfe0415
Vlad2088 <vladislavganieff@yandex.ru>
2023-07-23 15:19:10 +1200

    Defined associations in patterns according to ER-diagram

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :create_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+
   def tests_by_level(level)
     Test.joins('INNER JOIN tests_users ON tests.id = tests_users.test_id')
       .where(tests: { level: level}, tests_users: { user_id: id})
