6a42596f89d903992c85178e0efc4c32c0c897db
Vlad2088 <vladislavganieff@yandex.ru>
2023-07-26 23:51:58 +1200

    add scope methods and validation in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,16 @@ class Question < ApplicationRecord
   belongs_to :test
   
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :count_answer
+
+
+  private
+
+  def count_answer
+    return if  answers.count.between?(1,4)
+        
+    errors.add(:answers, 'Must have 1 to 4 answers')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
 
+  validates :email, presence: true, uniqueness: true
+
   def tests_by_level(level)
-    tests.where(level: level)
+    tests.by_level(level)
   end
 end
