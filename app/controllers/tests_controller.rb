cc46b19b92ef028a428c86d3773baeb6218f5d3e
Vlad2088 <vladislavganieff@yandex.ru>
2023-08-21 00:30:20 +1200

    delete callback set user

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,22 +1,17 @@
 class TestsController < ApplicationController
   before_action :set_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: %i[start]
 
   def index
     @tests = Test.all
   end
 
-  def show
-    
-  end
+  def show; end
 
   def new
     @test = Test.new
   end
 
-  def edit 
-    
-  end
+  def edit; end
 
   def create 
     @test = Test.new(test_params)
@@ -42,6 +37,8 @@ class TestsController < ApplicationController
   end
 
   def start
+    @user = User.first
+
     @user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
@@ -55,9 +52,4 @@ class TestsController < ApplicationController
   def set_test
     @test = Test.find(params[:id])
   end
-
-  def set_user
-    @user = User.first
-  end
-
 end
