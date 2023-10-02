f782c492294d03c0f43d4ea9b5bd24d8339ee6a1
Vlad2088 <vladislavganieff@yandex.ru>
2023-10-02 22:30:38 +1200

    refactoring_test_controller._Destroy_question_and_answer_controllers

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,23 +1,13 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test, only: %i[start]
 
   def index
     @tests = Test.all
   end
 
   def start
+    @test = Test.find(params[:id])
     current_user.tests.push(@test)
     redirect_to current_user.test_passage(@test)
-  end
-
-  private
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
-  end
-
-  def set_test
-    @test = Test.find(params[:id])
   end
 end
