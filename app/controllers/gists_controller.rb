4a7cfccf91b949b1122b83af47118a7cd00a7e2f
Vlad2088 <vladislavganieff@yandex.ru>
2023-10-23 13:15:20 +1200

    Refactoring_in_GistsController_method_create

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -4,7 +4,7 @@ class GistsController < ApplicationController
     @test_passage = TestPassage.find(params[:test_passage_id])
     @result = GistQuesnionServise.new(@test_passage.current_question).call
 
-    if @result.success
+    if @result.success == true
       @test_passage.current_question.gist.create!(user: current_user, url: @result.url)
       flash[:notice] = t('.succes_html', link: gist_link(@result.url))
     else
