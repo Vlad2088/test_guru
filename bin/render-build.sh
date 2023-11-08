2b50359059e116a9c4b1e91f36bc030d861902b8
Vlad2088 <vladislavganieff@yandex.ru>
2023-11-09 01:08:48 +1200

    refactoring_render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 bundle install
-yarn install
-bin/rails webpacker:install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bin/rails db:migrate
+bundle exec rake db:migrate
