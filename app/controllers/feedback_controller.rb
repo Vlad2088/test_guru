e943388ad2809a150d9e46e071d64913b9123e42
Vlad2088 <vladislavganieff@yandex.ru>
2023-11-11 14:55:13 +1200

    fixed an error in the name of the mailer class

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,7 +3,7 @@ class FeedbackController < ApplicationController
   def new; end
 
   def create
-    FeedbackMailer.feedback(params[:email], params[:name], params[:message]).deliver_now
+    FeedbacksMailer.feedback(params[:email], params[:name], params[:message]).deliver_now
     redirect_to root_path, notice: t('.success_feedback')
   end
 end
